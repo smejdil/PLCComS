e5b63ae588ea26cf694a71fc84ecfe7858d58d8e
root <Iam@LukasMaly.NET>
2017-03-12 12:32:20 +0100

    Update files

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-TECO="/opt/teco"
-TECO_CONF_DIR="/etc/teco"
-TECO_LOG_DIR="/var/log/teco"
+TECO_DIR="/opt/teco";
+TECO_CONF_DIR="/etc/teco";
+TECO_LOG_DIR="/var/log/teco";
 
 cd ${TECO_CONF_DIR}
-${TECO}/PLCComS_x86_64 -d -c ${TECO_CONF_DIR}/PLCComS.ini -l ${TECO_LOG_DIR}/PLCComS.log
+${TECO_DIR}/PLCComS_x86_64 -d -c ${TECO_CONF_DIR}/PLCComS.ini -l ${TECO_LOG_DIR}/PLCComS.log
 
 # EOF

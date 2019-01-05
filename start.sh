7a843e0d1001795da31d4a8e0a4a0594ecf72af9
Lukas Maly <Iam@LukasMaly.NET>
2019-01-05 12:15:46 +0100

    changes for running

--- start.sh
+++ start.sh
@@ -3,6 +3,10 @@
 TECO_DIR="/opt/teco";
 TECO_CONF_DIR="/etc/teco";
 TECO_LOG_DIR="/var/log/teco";
+TECO_LIB_DIR="$TECO_DIR/lib_x86_64";
+
+export MALLOC_CHECK_=4
+export LD_LIBRARY_PATH=$TECO_LIB_DIR
 
 cd ${TECO_CONF_DIR}
 exec ${TECO_DIR}/PLCComS_x86_64 -d -c ${TECO_CONF_DIR}/PLCComS.ini -l ${TECO_LOG_DIR}/PLCComS.log
